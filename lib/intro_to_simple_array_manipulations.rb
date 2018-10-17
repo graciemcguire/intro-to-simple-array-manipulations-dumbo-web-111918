86ebbe0fd4a91a52aff54def9786520d0c74b3e8
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 16:27:59 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,11 +31,11 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(3, element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
-  array.uniq()
+  array.uniq
 end
 
 def using_flatten(array)
@@ -43,4 +43,5 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
+  array.delete(string)
 end
