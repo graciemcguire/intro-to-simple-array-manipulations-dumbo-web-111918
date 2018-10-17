a1ceeafa80a9a213d9263a1d744b25a1a6fa426b
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 16:19:08 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,10 @@ def using_unahift(array, string)
   array.unshift(string)
 end 
 
-def using_pop(array, string)
+def using_pop(array)
   array.pop(string)
+end 
+
+def pop_with_args(array
+  array.unshift(string)
 end 
